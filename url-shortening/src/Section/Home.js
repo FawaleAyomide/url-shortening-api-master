c43fe6823eceb7c7406f2b1f504f8858d4bc21b3
Ayomide Oladele <fawaleayomideoladele@gmail.com>
2023-12-04 03:24:13 +0100

    style update

--- url-shortening/src/Section/Home.js
+++ url-shortening/src/Section/Home.js
@@ -4,6 +4,9 @@ import Nav from "./Nav";
 import Aos from "aos";
 import 'aos/dist/aos.css'
 import Hero from '../images/illustration-working.svg'
+import Recognition from '../images/icon-brand-recognition.svg'
+import Records from '../images/icon-detailed-records.svg'
+import Customizable from '../images/icon-fully-customizable.svg'
 
 const Home = () => {
 
@@ -31,39 +34,40 @@ const Home = () => {
     </div>
     </main>
     </header>
+
     <section>
         <div className="sect-1-cont">
-    <section>
         <div className="shorten" data-aos='zoom-in-up'>
         <input type="text" placeholder="Shorten a link here..." />
         <button className="btn">Shorten It!</button>
         </div>
-    </section>
         <div className="sect-1">
-            <div className="head">
+            <div className="head" data-aos='zoom-out-up' data-aos-delay='350'>
             <h1>Advanced Statistics</h1>
             <p>Track how your links are performing across the web with our 
             advanced statistics dashboard.
             </p>
             </div>
             <div className="box-cont">
-                <div className="box">
-                    Brand Recognition
-
-                    Boost your brand recognition with each click. Generic links don’t 
+                <div className="box" data-aos='zoom-out-left' data-aos-delay='1000'>
+                    <img src={Recognition} alt="icon" className="brand" />
+                    <h2>Brand Recognition</h2>
+                    <p>Boost your brand recognition with each click. Generic links don’t 
                     mean a thing. Branded links help instil confidence in your content.
+                    </p>
                 </div>
-                <div className="box">
-                    Detailed Records
-
-                    Gain insights into who is clicking your links. Knowing when and where 
+                <div className="box" data-aos='zoom-in-up' data-aos-delay='900'>
+                    <img src={Records} alt="icon" className="brand" />
+                    <h2>Detailed Records</h2>
+                    <p>Gain insights into who is clicking your links. Knowing when and where 
                     people engage with your content helps inform better decisions.
+                    </p>
                 </div>
-                <div className="box">
-                    Fully Customizable
-
-                    Improve brand awareness and content discoverability through customizable 
-                    links, supercharging audience engagement.
+                <div className="box" data-aos='zoom-out-right' data-aos-delay='800'>
+                    <img src={Customizable} alt="icon" className="brand" />
+                    <h2>Fully Customizable</h2>
+                    <p>Improve brand awareness and content discoverability through customizable 
+                    links, supercharging audience engagement.</p>
                 </div>
             </div>
         </div>
